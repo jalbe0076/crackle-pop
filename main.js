abedc23e3ccdbb7fd8d38bd7ab0aace2f824b4e8
Jason Alberto <alberto.jason@gmail.com>
2024-03-19 11:30:25 -0400

    Add logic to confirm a number is divisible by 5 and print Pop

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const cracklePop = () => {
   for (let i = 1; i <= 100; i++) {
     if (!(i % 3)) {
       console.log('Crackle')
+    } else if(!(i % 5)) {
+      console.log('Pop')
     } else {
       console.log(i)
     }
