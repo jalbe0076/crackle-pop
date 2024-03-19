75af825e46db14b9c08b4bf570e55cba2f280ae6
Jason Alberto <alberto.jason@gmail.com>
2024-03-19 11:29:08 -0400

    Add logic to confirm a number is divisible by 3 and print Crackle

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const cracklePop = () => {
   for (let i = 1; i <= 100; i++) {
-    console.log(i)
+    if (!(i % 3)) {
+      console.log('Crackle')
+    } else {
+      console.log(i)
+    }
   }
 }
 
