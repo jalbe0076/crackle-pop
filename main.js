ba2e5811a6be06e194ee57e15fc5d7cb06d3a00c
Jason Alberto <alberto.jason@gmail.com>
2024-03-19 11:32:25 -0400

    Add logic to confirm a number is divisible by 3 and 5 and print CracklePop

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const cracklePop = () => {
   for (let i = 1; i <= 100; i++) {
-    if (!(i % 3)) {
+    if (!(i % 3) && !(i % 5)) {
+      console.log('CracklePop')
+    } else if (!(i % 3)) {
       console.log('Crackle')
     } else if(!(i % 5)) {
       console.log('Pop')
